def9b83a0972ad00e3fb27cdfcb55b3825c261e5
Tanishq Thuse <tanishqthuse555@gmail.com>
2024-07-07 18:51:09 +0530

    Trie_Intro

---
+++ Trie_DSA_Fedora/code.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class code{
+    
+    static class Node{
+        Node[] children;
+        boolean endOfWord;
+
+        Node(){
+            children = new Node[26]; //a to z
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+            endOfWord = false;
+        }
+        
+    }
+
+    static Node root = new Node();
+    
+    public static void insert(String word){
+        Node curr = root;
+        
+        for(int i=0; i<word.length(); i++){
+            int index = word.charAt(i) - 'a';
+            if(curr.children[index]==null){
+                //We add a new node
+                root.children[index] = new Node();
+            }
+            if(i == word.length()-1){
+                curr.children[index].endOfWord = true;
+            }
+            root = root.children[index];
+        }
+    }
+
+    public static boolean search(String word){
+        Node curr = root;
+
+        for(int i=0; i<word.length(); i++){
+            int index = word.charAt(i) - 'a';
+            if(curr.children[index] == null){
+                // System.out.println("Word not found");
+                return false;
+            }
+            if(i==word.length()-1 && curr.children[index].endOfWord == false){
+                // System.out.println("Word not found");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Logic for this code is a bit not understood from recursion point of view
+    public static boolean wordBreak(String key){
+        if(key.length() == 0){
+            return true;
+        }
+        
+        for(int i=1; i<=key.length(); i++){
+            String firstPart = key.substrinng(0,i);
+            String secondPart = key.substring(i);
+            if(search(firstPart) && wordBreak()){
+                return true;
+            }
+        }
+    }
+
+
+    public static void main(){
+        
+        String words[] = {"hello", "world", "java", "code", "programming", "language"};
+        String key = "helloworld";
+
+    }
+}
